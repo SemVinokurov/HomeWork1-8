e3c6995963078bcb071fa824086278e95b35568d
Your Name <you@example.com>
2022-11-22 13:19:28 +0500

    The end

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿//Задача 1.
+/*
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if(n1 > n2)
+{
+    Console.WriteLine($"Maximum number: {n1}, Minimum number: {n2}");
+}
+else
+{
+    Console.WriteLine($"Maximum number: {n2}, Minimum number: {n1}");
+}
+*/
+
+//Задача 2.
+/*
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+if(n1 > n2)
+  if(n1>n3)
+  {
+   Console.WriteLine($"Maximum number: {n1}");
+  }
+  else
+  {
+    Console.WriteLine($"Maximum is number: {n3}");
+  }
+else
+ if(n2 > n3)
+ {
+    Console.WriteLine($"Maximum is number: {n2}");
+ }
+ else
+ {
+    Console.WriteLine($"Maximum is number: {n3}");
+ }
+*/
+//Задача 3
+/*
+Console.Write("Input a number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+if(n1 % 2 == 0)
+{
+    Console.WriteLine($"Even number: {n1}");
+}
+else
+{
+    Console.WriteLine("No!");
+}
+*/
+//Задача 4.
+/*Console.Write("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+
+while (current <= n)
+{
+    Console.Write(current + " ");
+    current += 2;
+}
+*/
